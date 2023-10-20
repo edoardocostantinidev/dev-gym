5cb8db9a92b9f486b24875fdb870bbb5afa8e6a4
Edoardo Costantini <edoardo.costantini@prima.it>
2023-10-20 14:43:55 +0200

    fix

--- rust/leetcode/l0001-two-sum/src/lib.rs
+++ rust/leetcode/l0001-two-sum/src/lib.rs
@@ -31,24 +31,6 @@ pub fn on_two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     vec![]
 }
 
-pub fn less_mem_two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    // build a map(value,index) with O(n)
-    let mut map: HashMap<i32, usize> = HashMap::new();
-
-    // search for target-n in map and retrieve indices, still O(n)
-    for (i, n) in nums.iter().enumerate() {
-        if let Some(j) = map.get(&(target - n)) {
-            if i != *j {
-                return vec![i as i32, *j as i32];
-            }
-        } else {
-            map.insert(*n, i);
-        }
-    }
-
-    panic!("impossible")
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,7 +43,7 @@ mod tests {
 
     #[test]
     fn test_on_two_sum() {
-        assert_eq!(on_two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
-        assert_eq!(on_two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+        assert_eq!(on_two_sum(vec![2, 7, 11, 15], 9), vec![1, 0]);
+        assert_eq!(on_two_sum(vec![3, 2, 4], 6), vec![2, 1]);
     }
 }
