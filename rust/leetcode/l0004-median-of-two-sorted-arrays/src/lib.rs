9a2cf17eeff08ed6d41b54394cebb0e83641591d
Edoardo Costantini <edoardo.costantini@prima.it>
2023-10-23 17:36:01 +0200

    l0004 100% runtime

--- rust/leetcode/l0004-median-of-two-sorted-arrays/src/lib.rs
+++ rust/leetcode/l0004-median-of-two-sorted-arrays/src/lib.rs
@@ -1,5 +1,6 @@
-use std::ops::Div;
+use std::{iter::Peekable, ops::Div, slice::Iter};
 
+//run 55% mem 43%
 pub fn naive(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
     let mut z = vec![];
     z.extend(nums1.into_iter());
@@ -14,54 +15,64 @@ pub fn naive(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
     }
 }
 
+//run 100% mem 99.88%
 pub fn fast(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
     let combined_length = nums1.len() + nums2.len();
-
-    let (longer, shorter) = if nums1.len() >= nums2.len() {
-        (nums1, nums2)
-    } else {
-        (nums2, nums1)
-    };
-
-    let mut longer_iter = longer.into_iter();
-    let mut shorter_iter = shorter.into_iter().peekable();
     let mut count = 0;
-    let mut last_value = i32::MIN;
     let middle = if combined_length % 2 == 0 {
         (Some((combined_length / 2, combined_length / 2 - 1)), None)
-    } else {
+    } else if combined_length > 2 {
         (None, Some(combined_length / 2))
+    } else {
+        (None, Some(1))
     };
-    dbg!(middle);
-    while let Some(n) = longer_iter.next() {
-        match middle {
-            (Some((mid, _)), _) if mid == count => return (n + last_value) as f64 / 2.0,
-            (None, Some(mid)) if mid == count => return n as f64,
-            _ => (),
-        }
-        dbg!((n, count, last_value), "before");
-
-        while let Some(m) = shorter_iter.peek() {
-            if *m < n {
-                count += 1;
-                match middle {
-                    (Some((mid, _)), _) if mid == count => return (m + last_value) as f64 / 2.0,
-                    (None, Some(mid)) if mid == count => return *m as f64,
-                    _ => (),
+    let mut p1 = nums1.iter().peekable();
+    let mut p2 = nums2.iter().peekable();
+    loop {
+        let x = get_next(&mut p1, &mut p2);
+        if let Some(num) = x {
+            match middle {
+                (None, Some(mid)) if count == mid - 1 => {
+                    return get_next(&mut p1, &mut p2).unwrap_or(num) as f64
                 }
-                last_value = *m;
-                shorter_iter.next();
-            } else {
-                count += 1;
-                last_value = n;
-                break;
+                (Some((mid, _)), None) if count == mid - 1 => {
+                    return (num + get_next(&mut p1, &mut p2).unwrap()) as f64 / 2.0
+                }
+                _ => {
+                    count += 1;
+                }
             }
+        } else {
+            return 0.0;
         }
-
-        dbg!((n, count, last_value), "after");
     }
+}
 
-    panic!("should not happen")
+fn get_next(p1: &mut Peekable<Iter<'_, i32>>, p2: &mut Peekable<Iter<'_, i32>>) -> Option<i32> {
+    let n1: Option<i32> = p1.peek().map(|x| **x);
+    let n2: Option<i32> = p2.peek().map(|x| **x);
+
+    match (n1, n2) {
+        (Some(n1), Some(n2)) => {
+            let result = if n1 < n2 {
+                p1.next();
+                n1
+            } else {
+                p2.next();
+                n2
+            };
+            Some(result)
+        }
+        (Some(n), _) => {
+            p1.next();
+            Some(n)
+        }
+        (_, Some(n)) => {
+            p2.next();
+            Some(n)
+        }
+        _ => None,
+    }
 }
 
 #[cfg(test)]
@@ -74,6 +85,7 @@ mod tests {
         // assert_eq!(naive(vec![1, 2], vec![3]), 2.0);
 
         assert_eq!(fast(vec![1, 2], vec![3, 4]), 2.5);
-        //assert_eq!(fast(vec![1, 2], vec![3]), 2.0);
+        assert_eq!(fast(vec![1, 2], vec![3]), 2.0);
+        assert_eq!(fast(vec![], vec![1]), 1.0);
     }
 }
