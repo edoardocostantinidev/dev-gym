704b5d082a15c408644ec8fb1026b554e31380e3
Edoardo Costantini <edoardo.costantini@prima.it>
2023-10-23 18:09:33 +0200

    wip

--- rust/leetcode/l0005-longest-palindromic-substring/src/lib.rs
+++ rust/leetcode/l0005-longest-palindromic-substring/src/lib.rs
@@ -1,5 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn naive(s: String) -> String {
+    let mut longest = String::new();
+    if s.len() == 1 {
+        return s;
+    } else if s.len() == 2 {
+        if s.chars().nth(0).unwrap() == s.chars().nth(1).unwrap() {
+            return s;
+        }
+    }
+    for (i, _) in s.chars().enumerate() {
+        for (j, _) in s.chars().enumerate() {
+            if i >= j {
+                continue;
+            }
+            let r = &s[i..j];
+            if is_palindrome(r) && r.len() > longest.len() {
+                longest = r.to_owned();
+            }
+        }
+    }
+    longest
+}
+
+pub fn is_palindrome(s: &str) -> bool {
+    let len = s.len();
+    match len {
+        0 | 1 => true,
+        2 => s.chars().nth(0).unwrap() == s.chars().nth(len - 1).unwrap(),
+        _ => {
+            s.chars().nth(0).unwrap() == s.chars().nth(len - 1).unwrap()
+                && is_palindrome(&s[1..len - 1])
+        }
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +39,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(naive(String::from("babad")), String::from("bab"));
+        assert_eq!(naive(String::from("cbbd")), String::from("bb"));
     }
 }
