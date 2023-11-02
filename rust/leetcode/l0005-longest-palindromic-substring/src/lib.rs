269b48cc4161cb94a1f8552bbcb08a5df261b08d
Edoardo Costantini <edoardo.costantini@prima.it>
2023-11-02 16:09:05 +0100

    l0005

--- rust/leetcode/l0005-longest-palindromic-substring/src/lib.rs
+++ rust/leetcode/l0005-longest-palindromic-substring/src/lib.rs
@@ -1,45 +1,31 @@
-pub fn naive(s: String) -> String {
-    let mut longest = String::new();
-    if s.len() == 1 {
-        return s;
-    } else if s.len() == 2 {
-        if s.chars().nth(0).unwrap() == s.chars().nth(1).unwrap() {
-            return s;
-        }
+// not mine, didn't know about windows() function
+pub fn longest_palindrome(s: String) -> String {
+    fn is_palidrome(s: &[u8]) -> bool {
+        s.iter().zip(s.iter().rev()).all(|(l, r)| l == r)
     }
-    for (i, _) in s.chars().enumerate() {
-        for (j, _) in s.chars().enumerate() {
-            if i >= j {
-                continue;
-            }
-            let r = &s[i..j];
-            if is_palindrome(r) && r.len() > longest.len() {
-                longest = r.to_owned();
-            }
-        }
-    }
-    longest
-}
 
-pub fn is_palindrome(s: &str) -> bool {
-    let len = s.len();
-    match len {
-        0 | 1 => true,
-        2 => s.chars().nth(0).unwrap() == s.chars().nth(len - 1).unwrap(),
-        _ => {
-            s.chars().nth(0).unwrap() == s.chars().nth(len - 1).unwrap()
-                && is_palindrome(&s[1..len - 1])
+    for size in dbg!(1..=s.len()).rev() {
+        match s
+            .as_bytes()
+            .windows(size)
+            .find(|substr| is_palidrome(substr))
+        {
+            Some(pal) => return String::from_utf8(pal.to_vec()).unwrap(),
+            None => continue,
         }
     }
+    String::from("")
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        assert_eq!(naive(String::from("babad")), String::from("bab"));
-        assert_eq!(naive(String::from("cbbd")), String::from("bb"));
+        assert_eq!(
+            longest_palindrome(String::from("babad")),
+            String::from("bab")
+        );
+        assert_eq!(longest_palindrome(String::from("cbbd")), String::from("bb"));
     }
 }
